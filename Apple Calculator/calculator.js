75c6abf05aadd32d622ad858438d600301feee64
Dusnorf King <125536610+Dusnorf-King@users.noreply.github.com>
2024-02-09 21:22:14 +0100

    Add files via upload

--- Apple Calculator/calculator.js
+++ Apple Calculator/calculator.js
@@ -1,14 +1,23 @@
+"use strict"
+
 function display(value) {
-  document.getElementById("display-result").value += value;
+  document.getElementById("displayResult").value += value;
 }
 
 function clearScreen() {
-  document.getElementById("display-result").value = "";
+  document.getElementById("displayResult").value = "";
 }
 
 function giveAns() {
-  let a = document.getElementById("display-result").value;
+  let a = document.getElementById("displayResult").value;
   let b = eval(a);
-  document.getElementById("display-result").value = b;
+  document.getElementById("displayResult").value = b;
 }
 
+function del() {
+  document.form.displayResult.value = document.form.displayResult.value.substr(0,form.displayResult.value.length-1)
+}
+
+function squareroot() {
+ 
+}
