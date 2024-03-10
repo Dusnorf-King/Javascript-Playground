2f837da4c96f37fa6c405602ec1b6cda7d594720
Dusnorf King <125536610+Dusnorf-King@users.noreply.github.com>
2024-03-10 15:54:48 +0100

    Add files via upload
    A digital clock that shows the time in real-time.

---
+++ DIGITAL CLOCK/Digiclock.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+const formatEl = document.getElementById("format");
+
+function getTime() {
+  let hr = new Date().getHours();
+  let min = new Date().getMinutes();
+  let sec = new Date().getSeconds();
+  let ampm = "PM";
+
+  if (hr > 12) {
+    hr = hr - 12;
+  } else {
+    ampm = "AM";
+  }
+
+  hr = hr < 10 ? "0" + hr : hr;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  hoursEl.textContent = hr;
+  minutesEl.textContent = min;
+  secondsEl.textContent = sec;
+  formatEl.textContent = ampm;
+
+  setTimeout(function () {
+    getTime();
+  }, 1000);
+}
+
+getTime();
