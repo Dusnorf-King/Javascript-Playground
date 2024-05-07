11d756bd909e7439cdaf15731e7b24a8c9d42f55
Dusnorf King <125536610+Dusnorf-King@users.noreply.github.com>
2024-05-07 18:55:01 +0100

    Add files via upload
    Fixed the Squareroot button

--- Apple Calculator/calculator.js
+++ Apple Calculator/calculator.js
@@ -1,23 +1,34 @@
-"use strict"
+"use strict";
+
+const getRoot = document.getElementById("squareroot");
+const displayResult = document.getElementById("displayResult");
 
 function display(value) {
-  document.getElementById("displayResult").value += value;
+  displayResult.value += value;
 }
 
 function clearScreen() {
-  document.getElementById("displayResult").value = "";
+  displayResult.value = "";
 }
 
 function giveAns() {
-  let a = document.getElementById("displayResult").value;
-  let b = eval(a);
-  document.getElementById("displayResult").value = b;
+  try {
+    let a = displayResult.value;
+    let b = eval(a);
+    displayResult.value = b;
+  } catch (error) {
+    displayResult.value = "Syntax Error";
+  }
 }
 
 function del() {
-  document.form.displayResult.value = document.form.displayResult.value.substr(0,form.displayResult.value.length-1)
+  document.form.displayResult.value = document.form.displayResult.value.substr(
+    0,
+    form.displayResult.value.length - 1
+  );
 }
 
-function squareroot() {
- 
-}
+
+getRoot.addEventListener("click", function () {
+  displayResult.value = Math.sqrt(displayResult.value);
+});
